b7cfbda8abcb9e91c6ec6556b37c8e68da5243d0
Thamaraiselvan <818589@cognizant.com>
2020-03-02 21:48:23 +0530

    kudu

--- deploy.cmd
+++ deploy.cmd
@@ -88,6 +88,11 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
+:: 0. Create empty lib\app.js to make KuduSync happy
+:: see https://github.com/projectkudu/kudu/issues/1753
+call :ExecuteCmd mkdir "%DEPLOYMENT_SOURCE%\lib"
+call :ExecuteCmd copy NUL "%DEPLOYMENT_SOURCE%\lib\app.js"
+
 :: 1. Select node version
 call :SelectNodeVersion
 
